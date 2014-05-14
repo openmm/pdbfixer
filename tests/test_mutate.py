9b51d5ba0902e2f108dcee2792c61e23425ce15a
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-05-14 15:00:21 -0400

    Removed extra return value.

--- tests/test_mutate.py
+++ tests/test_mutate.py
@@ -5,24 +5,24 @@ import tempfile
 
 def test_mutate_1():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
-    rmap = fixer.applyMutations(["ALA-16-GLY"])
+    fixer.applyMutations(["ALA-16-GLY"])
     app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
 
 
 def test_mutate_2():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
-    rmap = fixer.applyMutations(["ALA-16-GLY", "SER-15-ALA"])
+    fixer.applyMutations(["ALA-16-GLY", "SER-15-ALA"])
     app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
 
 @raises(ValueError)
 def test_mutate_3_fails():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
-    rmap = fixer.applyMutations(["ALA-15-GLY", "SER-15-ALA"])
+    fixer.applyMutations(["ALA-15-GLY", "SER-15-ALA"])
     app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
 
 
 @raises(KeyError)
 def test_mutate_4_fails():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
-    rmap = fixer.applyMutations(["ALA-16-WTF", "SER-15-ALA"])
+    fixer.applyMutations(["ALA-16-WTF", "SER-15-ALA"])
     app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
