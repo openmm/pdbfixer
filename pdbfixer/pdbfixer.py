04eaddb7c1b960df06dd5210f2833a38e4cf1a4c
Kyle Beauchamp <kyleabeauchamp@gmail.com>
2014-05-28 19:57:10 -0400

    Improved tests and docstring example.

--- pdbfixer/pdbfixer.py
+++ pdbfixer/pdbfixer.py
@@ -574,6 +574,8 @@ class PDBFixer(object):
         Find nonstandard residues.
 
         >>> fixer = PDBFixer(pdbid='1VII')
+        >>> fixer.findMissingAtoms()
+        >>> fixer.addMissingAtoms()        
         >>> fixer.applyMutations(["ALA-57-GLY"])
         >>> fixer.addMissingHydrogens(7.0)
 
--- tests/test_mutate.py
+++ tests/test_mutate.py
@@ -7,34 +7,34 @@ def test_mutate_1():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
     fixer.applyMutations(["ALA-57-GLY"], "A")
     fixer.addMissingHydrogens(7.0)
-    app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
+    temp_pdb = tempfile.NamedTemporaryFile()
+    app.PDBFile.writeFile(fixer.topology, fixer.positions, temp_pdb)
+    pdb = app.PDBFile(temp_pdb.name)
+    assert list(pdb.topology.residues())[16].name == "GLY", "Name of mutated residue did not change correctly!"
 
 
 def test_mutate_2():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
     fixer.applyMutations(["ALA-57-GLY", "SER-56-ALA"], "A")
     fixer.addMissingHydrogens(7.0)
-    app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
+    temp_pdb = tempfile.NamedTemporaryFile()
+    app.PDBFile.writeFile(fixer.topology, fixer.positions, temp_pdb)
+    pdb = app.PDBFile(temp_pdb.name)
+    assert list(pdb.topology.residues())[16].name == "GLY", "Name of mutated residue did not change correctly!"
+    assert list(pdb.topology.residues())[15].name == "ALA", "Name of mutated residue did not change correctly!"
 
 @raises(ValueError)
 def test_mutate_3_fails():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
     fixer.applyMutations(["ALA-57-GLY", "SER-57-ALA"], "A")
-    fixer.addMissingHydrogens(7.0)
-    app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
-
 
 @raises(KeyError)
 def test_mutate_4_fails():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
     fixer.applyMutations(["ALA-57-WTF", "SER-56-ALA"], "A")
-    fixer.addMissingHydrogens(7.0)
-    app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
 
 
 @raises(KeyError)
 def test_mutate_5_fails():
     fixer = pdbfixer.PDBFixer(pdbid='1VII')
     fixer.applyMutations(["ALA-1000-GLY", "SER-56-ALA"], "A")
-    fixer.addMissingHydrogens(7.0)
-    app.PDBFile.writeFile(fixer.topology, fixer.positions, tempfile.TemporaryFile())
