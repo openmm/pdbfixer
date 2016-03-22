f487261096222229d5a5ef652535992dd46c7f95
John Chodera (MSKCC) <choderaj@mskcc.org>
2016-03-22 11:29:56 -0700

    Refresh travis/devtools

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -1,17 +1,23 @@
+# Temporarily change directory to $HOME to install software
+pushd .
+cd $HOME
 
+# Install Miniconda
 MINICONDA=Miniconda-latest-Linux-x86_64.sh
+MINICONDA_HOME=$HOME/miniconda
 MINICONDA_MD5=$(curl -s http://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget http://repo.continuum.io/miniconda/$MINICONDA
+wget -q http://repo.continuum.io/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
 fi
-bash $MINICONDA -b
-PIP_ARGS="-U"
+bash $MINICONDA -b -p $MINICONDA_HOME
 
-export PATH=$HOME/miniconda/bin:$PATH
-
+# Configure miniconda
+export PIP_ARGS="-U"
+export PATH=$MINICONDA_HOME/bin:$PATH
 conda update --yes conda
-conda config --add channels http://conda.binstar.org/omnia
 conda install --yes conda-build jinja2 anaconda-client pip
 
+# Restore original directory
+popd
---
+++ devtools/travis-ci/push-docs-to-s3.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+
+"""
+Must have the vollowing environment variables defined:
+* BUCKET_NAME : AWS bucket name
+* PREFIX : 'latest' or other version number
+
+"""
+
+import os
+import pip
+import tempfile
+import subprocess
+import thermopyl.version
+
+
+BUCKET_NAME = 'thermopyl.org'
+if not thermopyl.version.release:
+    PREFIX = 'latest'
+else:
+    PREFIX = thermopyl.version.short_version
+
+if not any(d.project_name == 's3cmd' for d in pip.get_installed_distributions()):
+    raise ImportError('The s3cmd pacakge is required. try $ pip install s3cmd')
+# The secret key is available as a secure environment variable
+# on travis-ci to push the build documentation to Amazon S3.
+with tempfile.NamedTemporaryFile('w') as f:
+    f.write('''[default]
+access_key = {AWS_ACCESS_KEY_ID}
+secret_key = {AWS_SECRET_ACCESS_KEY}
+'''.format(**os.environ))
+    f.flush()
+
+    template = ('s3cmd --guess-mime-type --config {config} '
+                'sync docs/_build/ s3://{bucket}/{prefix}/')
+    cmd = template.format(
+            config=f.name,
+            bucket=BUCKET_NAME,
+            prefix=PREFIX)
+    return_val = subprocess.call(cmd.split())
+
+    # Sync index file.
+    template = ('s3cmd --guess-mime-type --config {config} '
+                'sync devtools/ci/index.html s3://{bucket}/')
+    cmd = template.format(
+            config=f.name,
+            bucket=BUCKET_NAME)
+    return_val = subprocess.call(cmd.split())
