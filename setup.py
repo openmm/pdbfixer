53b2d443f8df541df00b43a21f0be08ab4227805
peastman <peastman@stanford.edu>
2016-04-13 16:32:22 -0700

    Require OpenMM >= 7.0

--- setup.py
+++ setup.py
@@ -74,6 +74,6 @@ setup(
     packages=find_packages(),
     package_data={'pdbfixer': find_package_data()},
     zip_safe=False,
-    install_requires=['numpy', 'openmm >= 6.3'],
+    install_requires=['numpy', 'openmm >= 7.0'],
     entry_points={'console_scripts': ['pdbfixer = pdbfixer.pdbfixer:main']})
 
