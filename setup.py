c40688fa5879cb800d9888f3c4ea298ac302be89
peastman <peastman@stanford.edu>
2016-04-13 16:23:13 -0700

    Updated version number to 1.3

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ from setuptools import setup, find_packages
 DOCLINES = __doc__.split("\n")
 
 ########################
-__version__ = '1.2'
+__version__ = '1.3'
 VERSION = __version__
 ISRELEASED = False
 ########################
