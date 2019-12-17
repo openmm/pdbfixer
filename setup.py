e1a95798ad0fd132067d4aeafc2224544dc9f450
Bogdan Neterebskii <bog2dan1@gmail.com>
2019-12-17 08:11:55 +0300

    version 1.6: added Dockerfile; URL changed from `localhost` to `0.0.0.0`

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ from setuptools import setup, find_packages
 DOCLINES = __doc__.split("\n")
 
 ########################
-__version__ = '1.7'
+__version__ = '1.6'
 VERSION = __version__
 ISRELEASED = False
 ########################
