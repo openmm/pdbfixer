23cb4923d64ff853efa0d857ff0b105fc1cc82b0
peastman <peastman@stanford.edu>
2020-10-12 12:22:24 -0700

    Fix Travis builds

--- devtools/travis-ci/install.sh
+++ devtools/travis-ci/install.sh
@@ -1,12 +1,13 @@
 # Temporarily change directory to $HOME to install software
 pushd .
 cd $HOME
-
+# Make sure some level of pip is installed
+python -m ensurepip
 # Install Miniconda
-MINICONDA=Miniconda2-latest-Linux-x86_64.sh
+MINICONDA=Miniconda3-latest-Linux-x86_64.sh
 MINICONDA_HOME=$HOME/miniconda
-MINICONDA_MD5=$(curl -s https://repo.continuum.io/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
-wget -q https://repo.continuum.io/miniconda/$MINICONDA
+MINICONDA_MD5=$(wget -qO- https://repo.anaconda.com/miniconda/ | grep -A3 $MINICONDA | sed -n '4p' | sed -n 's/ *<td>\(.*\)<\/td> */\1/p')
+wget -q https://repo.anaconda.com/miniconda/$MINICONDA
 if [[ $MINICONDA_MD5 != $(md5sum $MINICONDA | cut -d ' ' -f 1) ]]; then
     echo "Miniconda MD5 mismatch"
     exit 1
@@ -15,9 +16,13 @@ bash $MINICONDA -b -p $MINICONDA_HOME
 
 # Configure miniconda
 export PIP_ARGS="-U"
-export PATH=$MINICONDA_HOME/bin:$PATH
-conda update --yes conda
-conda install --yes conda-build jinja2 anaconda-client pip
-
+# New to conda >=4.4
+echo ". $MINICONDA_HOME/etc/profile.d/conda.sh" >> ~/.bashrc  # Source the profile.d file
+echo "conda activate" >> ~/.bashrc  # Activate conda
+source ~/.bashrc  # source file to get new commands
+#export PATH=$MINICONDA_HOME/bin:$PATH  # Old way, should not be needed anymore
+conda config --set always_yes yes
+conda install conda conda-build jinja2 anaconda-client
+conda update --quiet --all
 # Restore original directory
 popd
