352426670735bb354281bf9dcee008b4d4f6bc25
sukritsingh <sukritsingh92@gmail.com>
2023-04-25 21:07:49 -0400

    Add SEP.pdb with CONECT records

--- pdbfixer/pdbfixer.py
+++ pdbfixer/pdbfixer.py
@@ -1129,7 +1129,7 @@ class PDBFixer(object):
         """Create a force field to use for optimizing the positions of newly added atoms."""
 
         if water:
-            forcefield = app.ForceField('amber14-all.xml', 'amber14/tip3p.xml')
+            forcefield = app.ForceField('amber14-all.xml', 'amber14/tip3p.xml', 'amber/phosaa14SB.xml')
             nonbonded = [f for f in forcefield._forces if isinstance(f, NonbondedGenerator)][0]
             radii = {'H':0.198, 'Li':0.203, 'C':0.340, 'N':0.325, 'O':0.299, 'F':0.312, 'Na':0.333, 'Mg':0.141,
                      'P':0.374, 'S':0.356, 'Cl':0.347, 'K':0.474, 'Br':0.396, 'Rb':0.527, 'I':0.419, 'Cs':0.605}
