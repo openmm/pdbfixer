cabdeb6becb3ba91fb543e92acc5adc6913c355a
Adrian Lange <alange@aalphabio.com>
2024-06-05 12:45:01 -0500

    numpy < 2 in setup.py

--- setup.py
+++ setup.py
@@ -74,6 +74,6 @@ setup(
     packages=find_packages(),
     package_data={'pdbfixer': find_package_data()},
     zip_safe=False,
-    install_requires=['numpy', 'openmm >= 7.1'],
+    install_requires=['numpy < 2', 'openmm >= 7.1'],
     entry_points={'console_scripts': ['pdbfixer = pdbfixer.pdbfixer:main']})
 
